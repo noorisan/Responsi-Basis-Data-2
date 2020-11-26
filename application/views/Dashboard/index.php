f0189b5bfc343ae31fe9154027fce3f69549c998
noorisan <noorisan3@gmail.com>
2020-11-26 22:19:16 +0700

    menambahkan card petugas Signed-off-by: noorisan <noorisan3@gmail.com>

--- application/views/Dashboard/index.php
+++ application/views/Dashboard/index.php
@@ -8,7 +8,7 @@
 
           <!-- Content Row -->
           <div class="row">
-            <div class="col-xl-4 col-md-6 mb-4">
+            <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -24,7 +24,7 @@
               </div>
             </div>
 
-            <div class="col-xl-4 col-md-6 mb-4">
+            <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -40,7 +40,23 @@
               </div>
             </div>
 
-            <div class="col-xl-4 col-md-6 mb-4">
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Data Petugas</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $petugas; ?> </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-users-cog fa-2x text-black-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
