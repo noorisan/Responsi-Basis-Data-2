67d36e0a0f2441befe9b3480acfe342f3d31b1c3
noorisan <noorisan3@gmail.com>
2020-11-27 01:33:38 +0700

    menerapkan stored function kedalam card data buku (dashboard) Signed-off-by: noorisan <noorisan3@gmail.com>

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -2,11 +2,11 @@
 	class Dashboard	extends CI_Controller{
 		public function index(){
 			$data['title']="Dashboard";
-			$buku=$this->db->query("SELECT * FROM buku");
+			$buku = $this->db->query("SELECT totalBuku()");
 			$anggota=$this->db->query("SELECT * FROM anggota");
 			$petugas=$this->db->query("SELECT * FROM petugas");
 			$peminjaman=$this->db->query("SELECT * FROM peminjaman");
-			$data['buku']=$buku->num_rows();
+			$data['buku']=$buku->unbuffered_row($type = 'array');
 			$data['anggota']=$anggota->num_rows();
 			$data['petugas']=$petugas->num_rows();
 			$data['peminjaman']=$peminjaman->num_rows();
--- application/views/Dashboard/index.php
+++ application/views/Dashboard/index.php
@@ -14,7 +14,7 @@
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Data Buku</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $buku; ?></div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $buku["totalBuku()"]; ?></div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-book fa-2x text-black-300"></i>
